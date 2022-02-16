77d52ba03649b411ba984f560e09fe5a9e4832b1
JideOgun <JideOgun@github.com>
2022-02-16 14:57:35 -0600

    added helpers to format date, and added partials

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+
 const path = require('path');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
